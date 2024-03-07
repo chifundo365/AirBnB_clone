c75fcbaa56b4a3319d77375ebe9f59df6f1a8410
= <=>
2024-03-07 10:43:13 -0800

    Base model tets

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from uuid import uuid4
 from models.base_model import BaseModel
 
+
 class Test_BaseModel(unittest.TestCase):
     """
     Defines different test case for the BaseModel class's
@@ -16,12 +17,12 @@ class Test_BaseModel(unittest.TestCase):
         self.base = BaseModel()
 
     def test__init__(self):
-        #test the id atrribute
+        # test the id atrribute
         self.assertNotEqual(self.base, BaseModel())
         self.assertNotEqual(self.base.id, BaseModel().id)
         self.assertEqual(len(self.base.id), len(BaseModel().id))
 
-        #test the created_at
+        # test the created_at
         self.assertIsInstance(self.base.created_at, datetime)
         self.assertEqual(self.base.created_at.minute,
                          BaseModel().created_at.minute)
@@ -29,6 +30,10 @@ class Test_BaseModel(unittest.TestCase):
                          BaseModel().created_at.month)
         self.assertEqual(self.base.created_at.day,
                          BaseModel().created_at.day)
+        self.assertEqual(self.base.created_at.second,
+                            BaseModel().created_at.second)
+        self.assertLessEqual(self.base.created_at,
+                            BaseModel().updated_at)
     
     def tearDown(self):
         pass
